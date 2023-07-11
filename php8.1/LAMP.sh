60b9b0f3a9b01c1cf34576940af5c8e67f3965e6
geekarmy <99156067+geekarmy@users.noreply.github.com>
2023-07-11 15:35:04 +0700

    Update LAMP.sh

--- php8.1/LAMP.sh
+++ php8.1/LAMP.sh
@@ -1,19 +1,19 @@
 #install PHP
 echo "Install PHP"
-apt install php8.1-fpm php8.1-dom php8.1-curl php8.1-mysql php8.1-gd php8.1-xml php8.1-bcmath php8.1-gmp php8.1-zip -y
+sudo apt install php8.1-fpm php8.1-dom php8.1-curl php8.1-mysql php8.1-gd php8.1-xml php8.1-bcmath php8.1-gmp php8.1-zip -y
 #install Composer
 echo "Install PHP"
 cd ~
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+sudo curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 echo $HASH
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 echo "check composer"
 composer
 #install git
 echo "Install Git"
-apt install git -y
+sudo apt install git -y
 #install nginx and cerbot
 echo "install Nginx"
 sudo apt update
